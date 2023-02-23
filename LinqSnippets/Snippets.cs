392ace9a828b5aef8b43e8eefa4e330782e004ec
Chanelp <mariannissanchez27@gmail.com>
2023-02-23 15:56:00 -0400

    Nuevos modelos e intro a Linq

---
+++ LinqSnippets/Snippets.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace LinqSnippets
+{
+
+    public class Snippets
+    {
+
+        static public void BasicLinQ()
+        {
+            string[] cars =
+            {
+                "VW Golf",
+                "VW California",
+                "Audi A3",
+                "Audi A5",
+                "Fiat Punto",
+                "Seat Ibiza",
+                "Seat León"
+            };
+
+            // 1. SELECT * of cars (SELECT ALL CARS)
+            var carList = from car in cars select car;
+
+            foreach(var car in carList)
+            {
+                Console.WriteLine(car);
+            }
+
+            // 2. SELECT WHERE car is Audi (SELECT AUDI's
+            var audiList = from car in cars where car.Contains("Audi") select car;
+
+            foreach (var audi in audiList)
+            {
+                Console.WriteLine(audi);
+            }
+
+        }
+
+        // 3. Number Examples
+        static public void LinqNumbers()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            // Each Number multiplied by 3
+            // take all numbers, but 9
+            // Order numbers by ascending
+
+            var processedNumberList = 
+                numbers
+                .Select(n => n * 3)
+                .Where(n => n != 9)
+                .OrderBy(n => n);
+
+
+        }
+
+    }
+}
---
+++ UniversityApiBackend/Models/DataModels/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Student : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public String Lastname { get; set; } = String.Empty;
+        [Required]
+        public DateTime Dob { get; set; }
+
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+
+    }
+}
